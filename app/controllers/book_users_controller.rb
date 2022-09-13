12b6b061f53c1d525adbdff16533839c44db026a
Michael Dao <mike@turing.edu>
2022-09-13 14:43:42 -0600

    refactors the book_users controller

--- app/controllers/book_users_controller.rb
+++ app/controllers/book_users_controller.rb
@@ -1,16 +1,9 @@
 class BookUsersController < ApplicationController
   def create
-      data = BookService.book(params[:isbn])
-      current_user.books << Book.where( 
-        title: data[:volumeInfo][:title],
-        authors: data[:volumeInfo][:authors].join(", "),
-        isbn: data[:volumeInfo][:industryIdentifiers][1][:identifier],
-        description: data[:volumeInfo][:description],
-        published_date: data[:volumeInfo][:publishedDate],
-        thumbnail: data[:volumeInfo][:imageLinks][:thumbnail]
-        )
-      .first_or_create
-
+    BookUser.where(
+      book: BookFacade.new(params[:isbn]), 
+      user: current_user
+      ).first_or_create
     redirect_to root_path
   end
 end
--- app/facades/book_facade.rb
+++ app/facades/book_facade.rb
@@ -4,7 +4,16 @@ class BookFacade
   end
 
   def book
-    service.book(@isbn) 
+    data = service.book(@isbn) 
+    Book.where( 
+      title: data[:volumeInfo][:title],
+      authors: data[:volumeInfo][:authors].join(", "),
+      isbn: data[:volumeInfo][:industryIdentifiers][1][:identifier],
+      description: data[:volumeInfo][:description],
+      published_date: data[:volumeInfo][:publishedDate],
+      thumbnail: data[:volumeInfo][:imageLinks][:thumbnail]
+      )
+    .first_or_create
   end
 
   def service
