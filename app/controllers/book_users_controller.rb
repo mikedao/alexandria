bf951b9bd8bc70f23f2500ecc90f69daeaed7154
Michael Dao <mike@turing.edu>
2022-09-13 14:44:16 -0600

    fixes typo

--- app/controllers/book_users_controller.rb
+++ app/controllers/book_users_controller.rb
@@ -1,7 +1,7 @@
 class BookUsersController < ApplicationController
   def create
     BookUser.where(
-      book: BookFacade.new(params[:isbn]), 
+      book: BookFacade.new(params[:isbn]).book, 
       user: current_user
       ).first_or_create
     redirect_to root_path
