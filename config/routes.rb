a37ead73e4ada54fc896b12dcfaba04f41bb8290
Michael Dao <mike@turing.edu>
2022-09-13 11:37:09 -0600

    users can be created and log in

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
   root 'welcome#index'
   resources :users, only: [:new, :create]
+  get '/login', to: 'sessions#new'
+  post '/login', to: 'sessions#create'
+  delete '/logout', to: 'sessions#destroy'
 end
---
+++ spec/features/user_authentication/user_can_log_in_spec.rb
@@ -0,0 +1,40 @@
+require 'rails_helper'
+
+RSpec.describe "Logging In" do
+  it "can log in with valid credentials" do
+    user = User.create(username: "funbucket13", password: "test", email: 'no@no.no')
+
+    visit root_path
+
+    click_on "Sign In"
+
+    expect(current_path).to eq('/login')
+
+    fill_in :username, with: user.username
+    fill_in :password, with: user.password
+
+    click_on "Log In"
+
+    expect(current_path).to eq(root_path)
+
+    expect(page).to have_content("Welcome, #{user.username}")
+  end
+
+  it "cannot log in with bad credentials" do
+    user = User.create(username: "funbucket13", 
+                        password: "test",
+                        email: "none@no.no")
+  
+    visit "/login"
+  
+    fill_in :username, with: user.username
+    fill_in :password, with: "incorrect password"
+  
+    click_on "Log In"
+  
+    expect(current_path).to eq(login_path)
+  
+    expect(page).to have_content("Sorry, your credentials are bad.")
+  end
+    
+end
